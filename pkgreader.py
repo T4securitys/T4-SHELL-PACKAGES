ac8d093f6aef20c3660f6b7cb7362e404a9b1eff
T4 Gamer <74420672+T4securitys@users.noreply.github.com>
2020-11-20 14:39:32 +0530

    Update and rename .gitnore to pkgreader.py

---
+++ pkgreader.py
@@ -0,0 +1,27 @@
+#!python
+#<==Build by==>
+#Swastik Sarkar
+#<==build version==>
+#version 1
+#<==support version==>
+#T4-SHELL-version1
+#<==WARNING==>
+#IT IS FOR EDUCATIONAL PURPOSE ONLY
+#PLEASE DO NOT FORK THIS FILE
+#THIS IS NOT FOR ANY FUN
+#-end-
+
+
+
+#<==PACKAGE READER==>
+
+import sys
+import os
+import date and time
+
+package_correction = True
+password_config = "Ss9192028292990"
+packages_correct_source = "com.google./T4-SHELL/auth.pkg.config/0/d-invisible.address/say-error/packages/verified"
+
+
+
